37864cd109f3741876e6b3eb886933fd1f1dbc87
Michael Tseng <matseng@gmail.com>
2014-05-29 12:35:14 -0700

    Completed MVP of form validation of an email address, but built-in angular filterfor email could be improved IMHO for partially entered email addresses

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ angular.module('MyApp')
   .controller('FormController', ['$scope', function($scope) {
     $scope.submit = function() {
       console.log("hello world", $scope.data);
+      $scope.myForm.submitted = true;
       if($scope.myForm.$valid) {
         console.log('The form is valid');
       } else {
